ec5f4eebbb84651bb561a020423a1d355c10fa5a
Chris Cable <ccable827@gmail.com>
2020-06-29 08:26:51 -0700

    rewrite code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,26 +8,34 @@ constructor(){
   super()
 
   this.videoRef = React.createRef()
+  this.turnOnVideo = this.turnOnVideo.bind(this)
+  this.handleVideo = this.handleVideo.bind(this)
+  this.videoError = this.videoError.bind(this)
+
 }
 
- componentDidMount(){
+ turnOnVideo(){
    navigator.mediaDevices.getUserMedia({video: true, audio: true})
      .then(this.handleVideo)
      .catch(this.videoError)
+
  }
 
- handleVideo = (stream) => {
+ handleVideo(stream){
    this.videoRef.current.srcObject = stream
  }
 
- videoError = (err) => {
+ videoError(err){
    alert(err.name)
  }
 
  render() {
    return (
+     <div>
      <video id="video-chat" autoPlay="true" ref={this.videoRef}>
      </video>
+     <button onClick={this.turnOnVideo}>turn on video</button>
+     </div>
    )
  }
 }
